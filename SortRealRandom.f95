94b64d9d69487d60bde7f1417cda8efc68eaf1eb
Juan R. Rodrigues <105756104+JuanRRodrigues@users.noreply.github.com>
2023-04-08 22:14:28 -0300

    Update SortRealRandom.f95

--- SortRealRandom.f95
+++ SortRealRandom.f95
@@ -1,3 +1,20 @@
+! Melhorias para fazer no código
+!1. Tornar o tamanho do array variável: atualmente o tamanho do array é fixo em 5. Uma melhoria seria permitir que 
+!o usuário digite o tamanho do array a ser ordenado.
+
+!2. Usar um algoritmo de ordenação mais eficiente: o Bubble Sort é um algoritmo simples, mas pode ser 
+!ineficiente em grandes conjuntos de dados. Para melhorar o desempenho do programa, 
+!seria recomendável usar um algoritmo de ordenação mais eficiente, como o Quick Sort ou o Merge Sort.
+
+!3. Verificar se o array está vazio ou tem apenas um elemento: o código atual não lida com
+!a possibilidade do array estar vazio ou ter apenas um elemento. Seria útil adicionar uma verificação 
+!para garantir que o array contenha pelo menos dois elementos antes de executar o algoritmo de ordenação.
+
+!4 .Adicionar comentários: embora o código seja relativamente simples, pode ser útil adicionar 
+!comentários para explicar cada seção do programa e tornar seu funcionamento mais claro.
+
+
+
 program sort
 implicit none
 integer :: i, contador, TAM
